5e1c7dfd743da7b9754b7b7405f704ef8b69ce52
Louis Dewa Voterra <sunrisegamingiphone@gmail.com>
2024-12-19 00:00:25 +0700

    random pokok e

---
+++ objectorientedprogramming.py
@@ -0,0 +1,10 @@
+class Mobil:
+    
+    warna = ""
+
+
+mobil_1 = Mobil()
+
+mobil_1.warna = "blue"
+
+print(mobil_1.warna)
---
+++ python_cobak.py
@@ -0,0 +1,25 @@
+# def luas_persegi(p,l):
+#     luas = p * l
+#     return luas
+
+# def kerucut(a,l,t):
+#     luas = 1/3 * a * l * t
+#     return luas
+
+# x = int(input("Masukkan nilai panjang : "))
+# y = int(input("Masukkan nilai lebar : "))
+
+# print(luas_persegi(5,3))
+
+# 10
+# a = int(input("Masukkan nilai alas : "))
+# l = int(input("Masukkan nilai lebar : "))
+# t = int(input("Masukkan nilai tinggi : "))
+
+
+# print("Luas Kerucut dari Alas = " + str(a) + " lebar = " + str(l) + " dan tinggi = " + str(t) + " adalah = " + str(kerucut(a,l,t)))
+
+import hello
+
+persegi_panjang = hello.mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang)
